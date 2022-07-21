42625bb43248db26f27494049e57d251b0e0ece4
DESKTOP-2IH9632\Marko <marko.lukesevik@protonmail.com>
2022-07-21 16:26:20 +0200

    added delete and put for tasks

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.get("/api/boards/:id/columns", (req, res) => {
   let boardColumns = columns.columns.find(
     (e) => e.boardId === parseInt(req.params.id)
   );
-  console.log(columns);
   res.send(boardColumns);
 });
 
@@ -33,5 +32,50 @@ app.get("/api/tasks/:id", (req, res) => {
   res.send(foundtask);
 });
 
-const port = process.env.PORT || 3000;
+app.put("/api/tasks/:id", (req, res) => {
+  let editedTask = tasks.tasks.find(
+    (task) => task.id === parseInt(req.params.id)
+  );
+
+  let copy = { ...req.body };
+
+  let currentColumn = columns.columns.find((column) => {
+    return column.boardId === req.body.boardId;
+  });
+  console.log(currentColumn);
+  let statusIndex = currentColumn.names.indexOf(req.body.status);
+  let counter = 0;
+  copy.subtasks.forEach((subtask) => {
+    if (subtask.isCompleted) counter++;
+  });
+
+  if (counter === copy.subtasks.length) {
+    copy.status = currentColumn.names[currentColumn.names.length - 1];
+  }
+  if (counter >= 1 && counter < currentColumn.names.length) {
+    copy.status = currentColumn.names[statusIndex + 1];
+  }
+
+  let editedTaskIndex = tasks.tasks.indexOf(editedTask);
+  tasks.tasks[editedTaskIndex] = copy;
+  res.send(copy);
+});
+
+app.delete("/api/tasks/:id", (req, res) => {
+  let filteredTasks = tasks.tasks.filter((task) => {
+    return task.id !== parseInt(req.params.id);
+  });
+  tasks.tasks = filteredTasks;
+  res.send(filteredTasks);
+});
+
+app.delete("/api/boards/:id", (req, res) => {
+  let filteredBoards = boards.boards.filter((board) => {
+    return board.id !== parseInt(req.params.id);
+  });
+  boards.boards = filteredBoards;
+  res.send(filteredBoards);
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`listening ${port}`));
